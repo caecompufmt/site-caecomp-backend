8dd3f842113023f77a2aded6df6af9e8a56d0e70
Cesar Sampaio <vinicius_spr@hotmail.com>
2021-12-31 01:33:12 -0400

    Feature + improvements

---
+++ test/integration/register.test.ts
@@ -0,0 +1,30 @@
+import 'mocha'
+import { expect } from 'chai'
+import { step } from 'mocha-steps'
+import faker from 'faker-br'
+import axios from 'axios'
+
+describe('User', () => {
+  let client 
+  before(async () => {
+    client = axios.create({
+      baseURL: `http://0.0.0.0:3333/`,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    const test = await client.get()
+    console.log('🚀 ~ file: auth.test.ts ~ line 17 ~ before ~ test', test.data)
+  })
+
+  step('User login', async function () {
+    const userPayload = {
+      email: faker.internet.email().toLowerCase(),
+      first_name: 'user name',
+      password: '123456@Cc',
+    }
+    const response = await client.post('api/register', userPayload)
+    expect(response.data.type).to.equal('bearer')
+    expect(response.status).to.equal(200)
+  })
+})
