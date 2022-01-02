33cf9a9f71e4b329995d42ea0dec03f7300aabaa
Cesar Sampaio <vinicius_spr@hotmail.com>
2022-01-02 17:14:45 -0400

    fix imports

--- config/auth.ts
+++ config/auth.ts
@@ -100,7 +100,7 @@ const authConfig: AuthConfig = {
         | that time.
         |
         */
-        model: () => import('App/Models/User'),
+        model: () => import('./../app/Models/User'),
       },
     },
   },
